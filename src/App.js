5ace855f738be67f5f5c10687223b8798f9ea322
Venaktasukumar <123547412+Venaktasukumar@users.noreply.github.com>
2023-03-16 10:49:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import Header from './components/Header';
 import Container from './components/Container';
 import data from './data';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, createContext } from 'react';
+import { useState, useMemo, createContext } from 'react';
 import Cart from "./components/Cart";
+
 const UseContext = createContext();
+
 function App() {
   const { products } = data;
   const [arrayLength, setarrayLength] = useState([]);
   const [count, setCount] = useState(0);
-  const [mainItems, setmainItems]=useState(products);
-  console.log(arrayLength);
-  console.log(count);
+  const [mainItems, setmainItems] = useState(products);
+
+  const contextValue = useMemo(() => {
+    return {
+      products,
+      arrayLength,
+      setarrayLength,
+      count,
+      setCount,
+      mainItems,
+      setmainItems
+    };
+  }, [products, arrayLength, count, mainItems]);
+
   return (
     <div className="App">
-      <UseContext.Provider value={{ products, arrayLength, setarrayLength, count, setCount,mainItems,setmainItems }}>
+      <UseContext.Provider value={contextValue}>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={
@@ -24,17 +37,22 @@ function App() {
               </>
             } />
           
-          <Route path='/cart' element={
-            <>
-            <Header />
-            <Cart />
-            </>
-          } />
+            <Route path='/cart' element={
+              <>
+                <Header />
+                <Cart />
+              </>
+            } />
           </Routes>
         </BrowserRouter>
       </UseContext.Provider>
     </div>
   );
 }
+
 export { UseContext };
 export default App;
+
+
+
+
